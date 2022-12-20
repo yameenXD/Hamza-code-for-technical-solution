62b3c0c977f17b76583f008127f9fcc0e7e7ae26
yameenXD <74267033+yameenXD@users.noreply.github.com>
2022-12-20 20:58:11 +0000

    Add files via upload

---
+++ Technical solution time/Quiz game design and coded solution/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_game_design_and_coded_solution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // this is the private method for the form1 load i.e the menu 
+        {
+            timer1.Start(); // this starts the timer for when the program runs
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label2.Text = DateTime.Now.ToLongTimeString();
+            label3.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var myform = new General_knowledge_question();
+            myform.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var myform = new Entertainment();
+            myform.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var myform = new History();
+            myform.Show();
+        }
+    }
+}
---
+++ Technical solution time/Quiz game design and coded solution/History.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_game_design_and_coded_solution
+{
+    public partial class History : Form
+    {
+        public History()
+        {
+            InitializeComponent();
+        }
+    }
+}
